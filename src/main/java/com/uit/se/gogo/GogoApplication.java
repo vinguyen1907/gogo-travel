cf8130e0aa2aef389fbe823ed1fdea1f74994717
Nguyễn Tiến Vĩ <94362297+vinguyen1907@users.noreply.github.com>
2024-09-28 19:39:58 +0700

    init project

---
+++ src/main/java/com/uit/se/gogo/GogoApplication.java
@@ -0,0 +1,13 @@
+package com.uit.se.gogo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GogoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GogoApplication.class, args);
+	}
+
+}
