7fbf218db3486c22b60d182ef550d691b37a8c72
Nguyễn Tiến Vĩ <94362297+vinguyen1907@users.noreply.github.com>
2024-10-12 21:58:40 +0700

    feat: compact version of search stay api

--- src/main/java/com/uit/se/gogo/service/impl/StayServiceImpl.java
+++ src/main/java/com/uit/se/gogo/service/impl/StayServiceImpl.java
@@ -2,10 +2,15 @@ package com.uit.se.gogo.service.impl;
 
 import com.uit.se.gogo.entity.Stay;
 import com.uit.se.gogo.repository.StayRepository;
+import com.uit.se.gogo.request.SearchStayRequest;
 import com.uit.se.gogo.service.StayService;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -14,6 +19,24 @@ public class StayServiceImpl implements StayService {
 
     @Override
     public Stay findById(String id) {
-        return stayRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("Stay not found"));
+        var stay = stayRepository.findById(id);
+        return stay.orElseThrow(() -> new EntityNotFoundException("Stay not found"));
+    }
+
+    @Override
+    public List<Stay> search(SearchStayRequest request) {
+        Pageable pageable = PageRequest.of(request.getPage(), request.getPageSize());
+        return stayRepository.search(
+//                request.getCheckinDate(),
+//                request.getCheckoutDate(),
+                request.getLocationId(),
+//                request.getRooms(),
+//                request.getGuests(),
+//                request.getMinPrice(),
+//                request.getMaxPrice(),
+                request.getRating(),
+                request.getType(),
+                pageable
+        );
     }
 }
