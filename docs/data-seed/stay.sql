5ad4b4bc0f340bc3331b6dda93967834ead9c6dd
Nguyễn Tiến Vĩ <94362297+vinguyen1907@users.noreply.github.com>
2024-11-04 23:07:49 +0700

    Update stay.sql

--- docs/data-seed/stay.sql
+++ docs/data-seed/stay.sql
@@ -1,11 +1,11 @@
-insert into MY_TABLE (id, first_name, last_name, birth)
-values  (10001, 'Georgi', 'Facello', 'M'),
-        (10002, 'Bezalel', 'Simmel', 'F'),
-        (10003, 'Parto', 'Bamford', 'M'),
-        (10004, 'Chirstian', 'Koblick', 'M'),
-        (10005, 'Kyoichi', 'Maliniak', 'M'),
-        (10006, 'Anneke', 'Preusig', 'F'),
-        (10007, 'Tzvetan', 'Zielinski', 'F'),
-        (10008, 'Saniya', 'Kalloufi', 'M'),
-        (10009, 'Sumant', 'Peac', 'F'),
-        (10010, 'Duangkaew', 'Piveteau', 'F');
+insert into public.stay (address, latitude, longitude, overview, rating, star_rating, stay_type, id, location_id)
+values  ('12 Ngô Quyền, Hoàn Kiếm, Hà Nội', 21.0285, 105.8542, 'Khách sạn sang trọng với dịch vụ hoàn hảo.', 4.5, 5, 'HOTEL', '1', '1'),
+        ('88 Đồng Khởi, Quận 1, Hồ Chí Minh', 10.7769, 106.7009, 'Khách sạn cao cấp gần trung tâm thương mại.', 4.2, 4, 'HOTEL', '2', '2'),
+        ('Nguyễn Tất Thành, Sơn Trà, Đà Nẵng', 16.0545, 108.2022, 'Khu nghỉ dưỡng với view biển tuyệt đẹp.', 4.8, 5, 'RESORT', '3', '3'),
+        ('Bãi Dài, Gành Dầu, Phú Quốc', 10.2899, 103.984, 'Khu nghỉ dưỡng với các dịch vụ spa và bãi biển riêng.', 4.6, 5, 'RESORT', '4', '7'),
+        ('01 Trần Hưng Đạo, Hội An', 15.8801, 108.338, 'Khách sạn cổ kính gần phố cổ Hội An.', 4, 3, 'HOTEL', '5', '6'),
+        ('02 Lê Lợi, Huế', 16.4637, 107.5909, 'Nhà nghỉ tiện lợi gần các địa điểm du lịch.', 3.9, 3, 'MOTEL', '6', '5'),
+        ('20 Nguyễn Văn Cừ, Cần Thơ', 10.0452, 105.7469, 'Nhà nghỉ giá rẻ với tiện nghi cơ bản.', 4.1, 4, 'MOTEL', '7', '9'),
+        ('Central Park West, New York', 40.7829, -73.9654, 'Khách sạn cao cấp đối diện Central Park.', 4.7, 5, 'HOTEL', '8', '13'),
+        ('1 Rue de Rivoli, Paris', 48.8566, 2.3522, 'Khách sạn sang trọng với view tháp Eiffel.', 4.9, 5, 'HOTEL', '9', '12'),
+        ('Sheikh Zayed Road, Dubai', 25.2048, 55.2708, 'Khu nghỉ dưỡng cao cấp với bể bơi vô cực.', 4.8, 5, 'RESORT', '10', '19');
