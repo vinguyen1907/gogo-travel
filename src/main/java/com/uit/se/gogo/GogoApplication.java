ad2a3a871dd70c7801c29303309751e2eb5cf78a
Lê Thành Lộc <106715293+thanhLoc1087@users.noreply.github.com>
2024-12-23 11:05:02 +0700

    deploy: revert some changes

--- src/main/java/com/uit/se/gogo/GogoApplication.java
+++ src/main/java/com/uit/se/gogo/GogoApplication.java
@@ -1,26 +1,35 @@
 package com.uit.se.gogo;
 
-import org.springframework.boot.SpringApplication;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.core.env.MapPropertySource;
+import org.springframework.core.env.MutablePropertySources;
+import org.springframework.core.env.StandardEnvironment;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import io.github.cdimascio.dotenv.DotenvEntry;
 
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class GogoApplication {
 
 	public static void main(String[] args) {
-		// Map<String, Object> env = Dotenv.load()
-		// 		.entries()
-		// 		.stream()
-		// 		.collect(
-		// 				Collectors.toMap(DotenvEntry::getKey, DotenvEntry::getValue));
-		// new SpringApplicationBuilder(GogoApplication.class)
-		// 		.environment(new StandardEnvironment() {
-		// 			@Override
-		// 			protected void customizePropertySources(MutablePropertySources propertySources) {
-		// 				super.customizePropertySources(propertySources);
-		// 				propertySources.addLast(new MapPropertySource("dotenvProperties", env));
-		// 			}
-		// 		}).run(args);
-		SpringApplication.run(GogoApplication.class, args);
+		Map<String, Object> env = Dotenv.load()
+				.entries()
+				.stream()
+				.collect(
+						Collectors.toMap(DotenvEntry::getKey, DotenvEntry::getValue));
+		new SpringApplicationBuilder(GogoApplication.class)
+				.environment(new StandardEnvironment() {
+					@Override
+					protected void customizePropertySources(MutablePropertySources propertySources) {
+						super.customizePropertySources(propertySources);
+						propertySources.addLast(new MapPropertySource("dotenvProperties", env));
+					}
+				}).run(args);
+		// SpringApplication.run(GogoApplication.class, args);
 	}
 }
