9f6b7ed7ceb2865ebf07f51885b442355e561c54
Lê Thành Lộc <106715293+thanhLoc1087@users.noreply.github.com>
2024-12-24 20:55:05 +0700

    fix bugu

--- src/main/java/com/uit/se/gogo/GogoApplication.java
+++ src/main/java/com/uit/se/gogo/GogoApplication.java
@@ -18,19 +18,19 @@ import io.github.cdimascio.dotenv.DotenvEntry;
 public class GogoApplication {
 
 	public static void main(String[] args) {
-		// Map<String, Object> env = Dotenv.load()
-		// 		.entries()
-		// 		.stream()
-		// 		.collect(
-		// 				Collectors.toMap(DotenvEntry::getKey, DotenvEntry::getValue));
-		// new SpringApplicationBuilder(GogoApplication.class)
-		// 		.environment(new StandardEnvironment() {
-		// 			@Override
-		// 			protected void customizePropertySources(MutablePropertySources propertySources) {
-		// 				super.customizePropertySources(propertySources);
-		// 				propertySources.addLast(new MapPropertySource("dotenvProperties", env));
-		// 			}
-		// 		}).run(args);
-		SpringApplication.run(GogoApplication.class, args);
+		Map<String, Object> env = Dotenv.load()
+				.entries()
+				.stream()
+				.collect(
+						Collectors.toMap(DotenvEntry::getKey, DotenvEntry::getValue));
+		new SpringApplicationBuilder(GogoApplication.class)
+				.environment(new StandardEnvironment() {
+					@Override
+					protected void customizePropertySources(MutablePropertySources propertySources) {
+						super.customizePropertySources(propertySources);
+						propertySources.addLast(new MapPropertySource("dotenvProperties", env));
+					}
+				}).run(args);
+		// SpringApplication.run(GogoApplication.class, args);
 	}
 }
