5e839d6deea1f0e48523189c2df7b210543508f6
Lê Thành Lộc <106715293+thanhLoc1087@users.noreply.github.com>
2024-12-28 14:22:25 +0700

    cors config

--- src/main/java/com/uit/se/gogo/config/SecurityConfiguration.java
+++ src/main/java/com/uit/se/gogo/config/SecurityConfiguration.java
@@ -53,9 +53,10 @@ public class SecurityConfiguration {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
         //Make the below setting as * to allow connection from any hos
-        corsConfiguration.setAllowedOrigins(List.of("http://localhost:8000"));
+        // corsConfiguration.setAllowedOrigins(List.of("http://localhost:8000"));
+        corsConfiguration.setAllowedOrigins(List.of("*"));
         corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
-        corsConfiguration.setAllowCredentials(true);
+        // corsConfiguration.setAllowCredentials(true);
         corsConfiguration.setAllowedHeaders(List.of("*"));
         corsConfiguration.setMaxAge(3600L);
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
--- src/main/java/com/uit/se/gogo/config/WebConfig.java
+++ src/main/java/com/uit/se/gogo/config/WebConfig.java
@@ -1,11 +1,10 @@
 package com.uit.se.gogo.config;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
+// @Configuration
 public class WebConfig {
 
     @Bean
