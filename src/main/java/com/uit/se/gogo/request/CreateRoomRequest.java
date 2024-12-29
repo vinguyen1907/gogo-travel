01fb99cfc1b1666aaf7f64f77d909482ecdbba2e
Nguyễn Tiến Vĩ <94362297+vinguyen1907@users.noreply.github.com>
2024-12-30 00:01:27 +0700

    feat: constraint create room room request fields

--- src/main/java/com/uit/se/gogo/request/CreateRoomRequest.java
+++ src/main/java/com/uit/se/gogo/request/CreateRoomRequest.java
@@ -1,6 +1,9 @@
 package com.uit.se.gogo.request;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PositiveOrZero;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,19 +13,28 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class CreateRoomRequest {
     @JsonProperty("owner_id")
+    @NotBlank
     private String ownerId;
+    @NotBlank
     private String name;
     @JsonProperty("stay_id")
+    @NotBlank
     private String stayId;
     @JsonProperty("base_fare")
+    @Positive
     private Double baseFare;
+    @PositiveOrZero
     private Double discount;
+    @PositiveOrZero
     private Double tax; // rate
     @JsonProperty("service_fee")
+    @PositiveOrZero
     private Double serviceFee;
     private String type;
     @JsonProperty("max_guests")
+    @Positive
     private Integer maxGuests;
     @JsonProperty("image_url")
+    @NotBlank
     private String imageUrl;
 }
